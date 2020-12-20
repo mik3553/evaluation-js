21c9f0fec9b9e78847694b6cdc380e4933cb79f2
mik3553 <sahraoui_ki@hotmail.com>
2020-12-20 09:46:22 +0100

    optimisation

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         { image: '5.jpg'}
     ]
 
-
     let state = 0; //initialiser un etat a 0
 
     const left = document.querySelector('.left');           // selection du bouton left
@@ -20,63 +19,48 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const sliders = document.querySelector('.container');  // selection du conteneur
     body.appendChild(nav);                                         // ajôut de la nav sur le body
 
-    // initialiser le container avec la premiere image du tableau
-    sliders.setAttribute('style', `background-image:url(./images/${images[state].image}`)
-
-    //generer les 4 boutons de navigation
-    for (let i = 0 ; i < images.length ; i++){
+    for (let i = 0 ; i < images.length ; i++){     //generer les 4 boutons de navigation
         const li = document.createElement('li');
-        li.classList = 'inactive';
+        li.classList.add('inactive');
         nav.appendChild(li);
     }
+    const navigation = document.querySelectorAll('.inactive');  // selection des li avec la class inactive
 
-    const li = document.querySelectorAll('.inactive');  // selection des li avec la class inactive
-    function addClass(state){     // ajouter une class au noeud du dom courant (image == state)
-        for (const [i,el] of li.entries()) {
-            if(i === state){
-                el.classList.add('active');
-            }
-        }
+    const synchronization = (state) => {   // fonction pour initialiser le conteneur avec la premiere image du tableau
+        sliders.setAttribute('style', `background-image:url(./images/${images[state].image}`);
+        changeClassName();
     }
 
-    function removeClass(){    // suppression une class au noeud du dom courant (image == state)
-        for (const el of li) {
+    for( let i = 0; i < navigation.length; i++) {
+        navigation[i].addEventListener('click', () => {
+            state = i;
+            synchronization(state);
+        },false)
+    }
+
+    const changeClassName = ()=> {    // suppression de la class inactive et ajout de la class active sur la navigation courante (li.position == state)
+        for (const el of navigation) {
             el.classList.remove('active');
         }
+        navigation[state].classList.add('active');
     }
 
     // fonction pour incrementer les images et retour a 0 si longueur du tableau === state
     right.addEventListener('click', () => {
         state++;
-        if(state === images.length){
-            state=0;
-        }
-        sliders.setAttribute('style', `background-image:url(./images/${images[state].image})`);
-        removeClass();
-        addClass(state);
+        state === images.length ? state = 0 : null;
+        synchronization(state);
     }, false)
 
     // fonction pour décrementer les images et retour à la derniere image si longueur du tableau < 0
     left.addEventListener('click', () => {
         state--;
-        if(state < 0 ){
-            state = images.length - 1;
-        }
-        sliders.setAttribute('style', `background-image:url(./images/${images[state].image})`);
-        removeClass();
-        addClass(state);
+        state < 0 ? state = images.length - 1 : null;
+        synchronization(state);
     },false)
 
-    const childs = nav.childNodes;
-    for( let i = 0; i < childs.length; i++) {
-        childs[i].addEventListener('click', (e) => {
-            removeClass();
-            state= i;
-            addClass(state);
-            sliders.setAttribute('style', `background-image:url(./images/${images[state].image})`);
+    synchronization(state);
 
-        },false)
-    }
 });
 
 
